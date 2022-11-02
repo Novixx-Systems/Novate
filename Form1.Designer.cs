4fa28e284bd65935ad23a7346342d9899c5ff779
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-11-02 10:30:57 +0100

    src

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Novate
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(775, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "[LANG1]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(648, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "[LANG2]";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 209);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(417, 150);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(371, 209);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(775, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Translate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Novate
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> femininePrefixes = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        Dictionary<string, string> masculinePrefixes = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        Dictionary<string, string> feminineWords = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        Dictionary<string, string> masculineWords = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+        Dictionary<string, string> words = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+        bool checkFem = false;
+        bool checkMas = false;
+
+        string src = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Init()
+        {
+            JArray o = JArray.Parse(src);
+            foreach (JObject item in o) // <-- Note that here we used JObject instead of usual JProperty
+            {
+                string name = item.GetValue("englishword").ToString();
+                string name2 = item.GetValue("translatedword").ToString();
+                string type = item.GetValue("type").ToString();
+                if (type == "prefixFeminine")
+                {
+                    femininePrefixes.Add(name, name2);
+                }
+                else if (type == "prefixMasculine")
+                {
+                    masculinePrefixes.Add(name, name2);
+                    words.Add(name, name2);
+                }
+                else if (type == "masculine")
+                {
+                    masculineWords.Add(name, name2);
+                }
+                else if (type == "feminine")
+                {
+                    feminineWords.Add(name, name2);
+                }
+                else if (type == "default")
+                {
+                    words.Add(name, name2);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                src = System.IO.File.ReadAllText(openFileDialog.FileName);
+                Init();
+            }
+        }
+        void Trans()
+        {
+
+            List<string> strs = new List<string>();
+            foreach (string b in richTextBox1.Text.Split(" ").Reverse())
+            {
+                string c = b.Replace("?", " ?").Replace("!", " !").Replace(",", " ,").Replace(".", " .");
+                string a = c.Split(" ")[0];
+                if (feminineWords.ContainsKey(a))
+                {
+                    checkFem = true;
+                    strs.Add(ReplaceCaseInsensitive(b, a, feminineWords[a]));
+                    continue;
+                }
+                else if (masculineWords.ContainsKey(a))
+                {
+                    checkMas = true;
+                    strs.Add(ReplaceCaseInsensitive(b, a, masculineWords[a]));
+                    continue;
+                }
+                if (checkMas)
+                {
+                    if (masculinePrefixes.ContainsKey(a))
+                    {
+                        strs.Add(ReplaceCaseInsensitive(b, a, masculinePrefixes[a]));
+                        checkMas = false;
+                    }
+                }
+                else if (checkFem)
+                {
+                    if (femininePrefixes.ContainsKey(a))
+                    {
+                        strs.Add(ReplaceCaseInsensitive(b, a, femininePrefixes[a]));
+                        checkFem = false;
+                    }
+                }
+                else if (words.ContainsKey(a))
+                {
+                    strs.Add(ReplaceCaseInsensitive(b, a, words[a]));
+                }
+                else if (b == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    strs.Add(b);
+                }
+            }
+            strs = strs.ToArray().Reverse().ToList();
+            richTextBox2.Text = string.Join(' ', strs);
+        }
+        static string ReplaceCaseInsensitive(string Text, string Find, string Replace)
+        {
+            char[] NewText = Text.ToCharArray();
+            int ReplaceLength = Math.Min(Find.Length, Replace.Length);
+
+            int LastIndex = -1;
+            while (true)
+            {
+                LastIndex = Text.IndexOf(Find, LastIndex + 1, StringComparison.CurrentCultureIgnoreCase);
+
+                if (LastIndex == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < ReplaceLength; i++)
+                    {
+                        if (NewText.Length > Replace.Length)
+                        {
+                            NewText = NewText.Take(NewText.Length - 1).ToArray();
+                        }
+                        if (char.IsUpper(Text[i + LastIndex]))
+                            NewText[i + LastIndex] = char.ToUpper(Replace[i]);
+                        else
+                            NewText[i + LastIndex] = char.ToLower(Replace[i]);
+                    }
+                }
+            }
+
+            return new string(NewText);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Trans();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Trans();
+        }
+    }
+}
